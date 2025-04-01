b367e66dfb1fdc7533f513fa617328300b56b6a7
Ubukaze1 <gabo2060@hotmail.com>
2025-03-31 22:24:30 -0500

    feat(payroll): final del payroll

--- src/payroll/payroll.controller.ts
+++ src/payroll/payroll.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
+
+import { CreatePayrollDto, FiltersPayrollDto, UpdatePayrollDto } from './dto';
+
 import { PayrollService } from './payroll.service';
-import { CreatePayrollDto } from './dto/create-payroll.dto';
-import { UpdatePayrollDto } from './dto/update-payroll.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { Payroll } from './entities/payroll.entity';
 
 @Controller('payroll')
 export class PayrollController {
-  constructor(private readonly payrollService: PayrollService) {}
+  constructor(private readonly payrollService: PayrollService) { }
 
+  @ApiResponse({ description: 'Create a new payroll record', status: 200, type: Payroll })
+  @ApiResponse({ description: 'Bad Request', status: 400 })
+  @ApiResponse({ description: 'Internal Server Error', status: 500 })
   @Post()
-  create(@Body() createPayrollDto: CreatePayrollDto) {
-    return this.payrollService.create(createPayrollDto);
+  async create(@Body() data: CreatePayrollDto) {
+    return this.payrollService.create(data);
   }
 
+  @ApiResponse({ description: 'List all payroll records', status: 200, type: Payroll, isArray: true })
+  @ApiResponse({ description: 'Not Found', status: 404 })
+  @ApiResponse({ description: 'Bad Request', status: 400 })
   @Get()
-  findAll() {
-    return this.payrollService.findAll();
+  async list(@Query() filter: FiltersPayrollDto) {
+    return this.payrollService.list(filter);
   }
 
+  @ApiResponse({ description: 'Get a payroll record by ID', status: 200, type: Payroll })
+  @ApiResponse({ description: 'Not Found', status: 404 })
+  @ApiResponse({ description: 'Bad Request', status: 400 })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.payrollService.findOne(+id);
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.payrollService.findOne(id);
   }
 
+  @ApiResponse({ description: 'Update a payroll record', status: 200, type: Payroll })
+  @ApiResponse({ description: 'Not Found', status: 404 })
+  @ApiResponse({ description: 'Bad Request', status: 400 })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePayrollDto: UpdatePayrollDto) {
-    return this.payrollService.update(+id, updatePayrollDto);
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() data: UpdatePayrollDto) {
+    return this.payrollService.update(id, data);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.payrollService.remove(+id);
-  }
+  // @Delete(':id') we can´t delete a payroll
+  // remove(@Param('id') id: string) {
+  //   return this.payrollService.remove(+id);
+  // }
 }
